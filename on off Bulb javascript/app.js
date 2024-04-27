6d373edea75a36b830d92421fbb04a06c94be035
jameel shaikh <157943752+shaikhjameel@users.noreply.github.com>
2024-04-27 10:32:59 +0500

    Add files via upload

---
+++ on off Bulb javascript/app.js
@@ -0,0 +1,9 @@
+function bulbOn(){
+    let bulbobj = document.querySelector(".bulboff");
+    bulbobj.setAttribute("src", "bulb-on.jpg");
+}
+
+function bulbOff(){
+    let bulbobj = document.querySelector(".bulboff");
+    bulbobj.setAttribute("src", "bulb-off.jpg");
+}
